da49c4084b85aeeb1971ba5a46cd7bcd0ed0d0f8
anmatika <4ntti.matikainen@gmail.com>
2012-08-12 11:12:44 +0300

    no comments

---
+++ app/models/links.rb
@@ -0,0 +1,10 @@
+class Links
+
+  def self.all
+    {"http://www.google.fi" => "Google",
+     "https://www.facebook.com" => "Facebook"}
+  end
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Homepage::Application.routes.draw do
   resources :guestposts
   resources :'home'
+  resources :links
   root :to => 'home#index'
 end
